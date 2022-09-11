88713b0d2726be17d5d8950fd571f773d78465fb
Gonzalo Reynoso <gonzalo.reynoso9@gmail.com>
2022-09-11 19:13:23 -0300

    arreglo enlaces

--- javascript/main.js
+++ javascript/main.js
@@ -6,8 +6,6 @@ function introducirClave(){
 	let claveIngresada = prompt("Ingrese su clave");
 	let intento = 0;
 
-
-
 	while (claveIngresada != "celular") {
 		
 		intento++
@@ -15,10 +13,40 @@ function introducirClave(){
 		claveIngresada = prompt("Ingrese palabra clave");
 	}
 
-	if(claveIngresada == "celular"){
+	if(claveIngresada === "celular"){
 		alert("Código correcto - Bienvenido" )
 		}
 
 }
 
 introducirClave();
+
+
+// function calcularPrecios(){
+
+
+// 	let productoIngresado = prompt('Ingrese un producto de la lista, el precio final acumulado será mostrado al final',' $10 termo  $5 mate o $2 bombilla');
+// 	let respuesta = "si";
+// 	let sumaDeTotales = 1;
+
+
+
+
+// 	while (respuesta == "si") {
+// 		while (productoIngresado != "termo" || productoIngresado != "mate" != productoIngresado != "bombilla"){
+// 			productoIngresado = prompt("Error, reingrese productos de la lista","termo mate o bombilla");
+// 		}
+		
+// 	}
+
+// 	contadorTermo = productoIngresado 
+
+// 	sumaDeTotales = 
+
+
+// 	alert(productoIngresado);
+// }
+
+// calcularPrecios();
+
+
