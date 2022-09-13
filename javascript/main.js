c842ffce5ecdf711a85f638e9b08490b5933fa3e
Gonzalo Reynoso <gonzalo.reynoso9@gmail.com>
2022-09-13 15:04:06 -0300

    Simulador interactivo

--- javascript/main.js
+++ javascript/main.js
@@ -1,52 +1,116 @@
 //Primer desafío entregable Gonzalo Reynoso
 
 
-function introducirClave(){
+// function introducirClave(){
 
-	let claveIngresada = prompt("Ingrese su clave");
-	let intento = 0;
+// 	let claveIngresada = prompt("Ingrese su clave");
+// 	let intento = 0;
 
-	while (claveIngresada != "celular") {
-		
-		intento++
-		alert("Código incorrecto" + " Pista: celular" + " Intento Nro" + intento);
-		claveIngresada = prompt("Ingrese palabra clave");
-	}
+// 	while (claveIngresada != "celular") {
 
-	if(claveIngresada === "celular"){
-		alert("Código correcto - Bienvenido" )
-		}
+// 		intento++
+// 		alert("Código incorrecto" + " Pista: celular" + " Intento Nro" + intento);
+// 		claveIngresada = prompt("Ingrese palabra clave");
+// 	}
+
+// 	if(claveIngresada === "celular"){
+// 		alert("Código correcto - Bienvenido" )
+// 		}
+
+// }
+
+// introducirClave();
+
+//DESAFIO ENTREGABLE SIMULADOR INTERACTIVO GONZALO REYNOSO
+
+
+function calcularPrecios() {
+
+
+  let productoIngresado;
+  let respuesta = "si";
+  let sumaFinalPrecios;
+  let precioTermo = 10;
+  let precioMate = 5;
+  let precioBombilla = 2;
+  let contadorTermo = 0;
+  let contadorMate = 0;
+  let contadorBombilla = 0;
+  let descuentoDel10 = 10;
+  let precioFinalTermo;
+  let precioFinalMate;
+  let precioFinalBombilla;
+  let mensajeDescuento;
+  let mensaje;
+  let cantidadTotalDeProductos;
+  let precioConDescuento;
+  let precioFinal;
+
+
+
+
+  while (respuesta == "si") {
+    productoIngresado = prompt(`Ingrese un producto de la lista, el precio final acumulado será mostrado al final,  $10 termo  $5 mate o $2 bombilla`);
+    while (productoIngresado != "termo" && productoIngresado != "mate" && productoIngresado != "bombilla") {
+      productoIngresado = prompt("Error, reingrese productos de la lista", "termo mate o bombilla");
+    }
+
+    //CONTADORES
+    switch (productoIngresado) {
+      case "termo":
+        contadorTermo++;
+        break;
+
+      case "mate":
+        contadorMate++;
+        break;
+
+      case "bombilla":
+        contadorBombilla++;
+        break;
+
+    }
+    alert('Si compra 3 unidades o mas en total de cualquier item, tendrá un 10% de descuento!');
+    respuesta = prompt("¿Desea seguir agregando ventas?" + 'si,no');
+    while (respuesta != "si" && respuesta != "no") {
+      respuesta = prompt('Ingrese si o no por favor únicamente');
+    }
+
+  }//FIN WHILE
+
+
+  precioFinalTermo = precioTermo * contadorTermo;
+  precioFinalMate = precioMate * contadorMate;
+  precioFinalBombilla = precioBombilla * contadorBombilla;
+
+  sumaFinalPrecios = precioFinalTermo + precioFinalMate + precioFinalBombilla;
+
+  cantidadTotalDeProductos = contadorTermo + contadorMate + contadorBombilla;
+
+
+  if (cantidadTotalDeProductos >= 3) {
+    mensajeDescuento = 'Felicidades, por su compra obtuvo un 10% de descuento';
+    precioConDescuento = sumaFinalPrecios/descuentoDel10;
+  }
+  else if (cantidadTotalDeProductos == 2) {
+    mensajeDescuento = 'Compre 3 unidades o mas y obtendrá un 10% de descuento';
+    precioConDescuento=0;
+  } else {
+    mensajeDescuento = 'No hay descuento';
+    precioConDescuento=0;
+  }
+
+
+  precioFinal = sumaFinalPrecios - precioConDescuento;
+
+  //MENSAJE FINAL
+  mensaje = `Usted compró ${contadorTermo} unidad/es de termos, <br>
+  ${contadorMate} unidad/es de mates,<br>
+  ${contadorBombilla} unidad/es de bombillas,<br>
+  ${mensajeDescuento} El precio final a pagar es de ${precioFinal}`;
+
+  document.write(mensaje);
 
 }
 
-introducirClave();
-
-
-// function calcularPrecios(){
-
-
-// 	let productoIngresado = prompt('Ingrese un producto de la lista, el precio final acumulado será mostrado al final',' $10 termo  $5 mate o $2 bombilla');
-// 	let respuesta = "si";
-// 	let sumaDeTotales = 1;
-
-
-
-
-// 	while (respuesta == "si") {
-// 		while (productoIngresado != "termo" || productoIngresado != "mate" != productoIngresado != "bombilla"){
-// 			productoIngresado = prompt("Error, reingrese productos de la lista","termo mate o bombilla");
-// 		}
-		
-// 	}
-
-// 	contadorTermo = productoIngresado 
-
-// 	sumaDeTotales = 
-
-
-// 	alert(productoIngresado);
-// }
-
-// calcularPrecios();
-
-
+calcularPrecios();
