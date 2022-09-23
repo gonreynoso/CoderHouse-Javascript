2a4d193c0f2f1ea61d88a6e20f60893176f517be
Gonzalo Reynoso <gonzalo.reynoso9@gmail.com>
2022-09-22 21:19:55 -0300

    1er entrega final

--- javascript/main.js
+++ javascript/main.js
@@ -1,116 +1,133 @@
-//Primer desafío entregable Gonzalo Reynoso
 
+//1er pre entrega del final e commerce Gonzalo Reynoso
 
-// function introducirClave(){
+//VARIABLES
 
-// 	let claveIngresada = prompt("Ingrese su clave");
-// 	let intento = 0;
+let productoIngresado;
+let sumaFinalPrecios;
+let precioTermo = 40;
+let precioMate = 30;
+let precioBombilla = 10;
+let contadorTermo = 0;
+let contadorMate = 0;
+let contadorBombilla = 0;
+let descuentoDel10 = 10;
+let precioFinalTermo;
+let precioFinalMate;
+let precioFinalBombilla;
+let mensajeDescuento;
+let mensaje;
+let mostrar;
+let productos;
+let articulos;
+let cantidadTotalDeProductos;
+let precioConDescuento;
+let precioFinal;
+let productoEncontrado;
 
-// 	while (claveIngresada != "celular") {
+    //Buscador de productos
+    function buscarProductos(){
+        alert(`Buscador de productos`);
+        productos = [
+            {id: 1, nombre:'termo', precio: 40, marca: 'Stanley', presentacion: '950 ml'},
+            {id: 2, nombre:'mate', precio: 30, marca: 'Stanley', presentacion: '250 ml'},
+            {id: 3, nombre:'bombilla', precio: 10, marca: 'Stanley', presentacion: '18 cm'},
+        ]
+        
+        productoIngresado = prompt('Ingrese un producto, termo, mate o bombilla');
+        productoEncontrado = productos.find((articulos) => articulos.nombre === productoIngresado);
 
-// 		intento++
-// 		alert("Código incorrecto" + " Pista: celular" + " Intento Nro" + intento);
-// 		claveIngresada = prompt("Ingrese palabra clave");
-// 	}
+        mensaje= `Producto:
+        ${productoEncontrado.nombre}
+        ${productoEncontrado.marca}
+        ${productoEncontrado.presentacion}
+        Precio $${productoEncontrado.precio}`
 
-// 	if(claveIngresada === "celular"){
-// 		alert("Código correcto - Bienvenido" )
-// 		}
+        alert(mensaje);
 
-// }
-
-// introducirClave();
-
-//DESAFIO ENTREGABLE SIMULADOR INTERACTIVO GONZALO REYNOSO
-
-
-function calcularPrecios() {
-
-
-  let productoIngresado;
-  let respuesta = "si";
-  let sumaFinalPrecios;
-  let precioTermo = 10;
-  let precioMate = 5;
-  let precioBombilla = 2;
-  let contadorTermo = 0;
-  let contadorMate = 0;
-  let contadorBombilla = 0;
-  let descuentoDel10 = 10;
-  let precioFinalTermo;
-  let precioFinalMate;
-  let precioFinalBombilla;
-  let mensajeDescuento;
-  let mensaje;
-  let cantidadTotalDeProductos;
-  let precioConDescuento;
-  let precioFinal;
-
-
-
-
-  while (respuesta == "si") {
-    productoIngresado = prompt(`Ingrese un producto de la lista, el precio final acumulado será mostrado al final,  $10 termo  $5 mate o $2 bombilla`);
-    while (productoIngresado != "termo" && productoIngresado != "mate" && productoIngresado != "bombilla") {
-      productoIngresado = prompt("Error, reingrese productos de la lista", "termo mate o bombilla");
+        let = respuesta=prompt('Desea buscar otro producto?Ingrese si sino no')
+        while (respuesta==='si') {
+            buscarProductos();
+        }
     }
 
-    //CONTADORES
-    switch (productoIngresado) {
-      case "termo":
-        contadorTermo++;
-        break;
+    //Ingresar productos
+    function ingresarProductos() {
+        let= respuesta = "si"
+        while (respuesta == "si"){ 
+            productoIngresado = prompt("Ingrese un producto de la lista, el precio final acumulado será mostrado al final, $40 termo  $30 mate o $10 bombilla");
 
-      case "mate":
-        contadorMate++;
-        break;
-
-      case "bombilla":
-        contadorBombilla++;
-        break;
-
-    }
-    alert('Si compra 3 unidades o mas en total de cualquier item, tendrá un 10% de descuento!');
-    respuesta = prompt("¿Desea seguir agregando ventas?" + 'si,no');
-    while (respuesta != "si" && respuesta != "no") {
-      respuesta = prompt('Ingrese si o no por favor únicamente');
+            while (productoIngresado != "termo" && productoIngresado != "mate" && productoIngresado !="bombilla"){
+                productoIngresado = prompt("Error, reingrese productos de la lista", "termo mate o bombilla");
+            }
+            respuesta = prompt("¿Desea seguir agregando ventas?" + "si,no");
+            while (respuesta != "si" && respuesta != "no") {
+                respuesta = prompt("Ingrese si o no por favor únicamente");
+            }    
+            //Se aplica la funcion contadores
+            contadores();
+        }
     }
 
-  }//FIN WHILE
+    //Proceso de contar y guardar cantidad     CONTADORES
+    function contadores(){
+        switch (productoIngresado) {
+        case "termo":
+            contadorTermo++;
+            break;
+    
+        case "mate":
+            contadorMate++;
+            break;
+    
+        case "bombilla":
+            contadorBombilla++;
+            break;
+        }
+    }
 
+    //Lógica de productos
+    function calcularProductos() {
+        precioFinalTermo = precioTermo * contadorTermo;
+        precioFinalMate = precioMate * contadorMate;
+        precioFinalBombilla = precioBombilla * contadorBombilla;
+        
+        sumaFinalPrecios = precioFinalTermo + precioFinalMate + precioFinalBombilla;
+        
+        cantidadTotalDeProductos = contadorTermo + contadorMate + contadorBombilla;
+    }
 
-  precioFinalTermo = precioTermo * contadorTermo;
-  precioFinalMate = precioMate * contadorMate;
-  precioFinalBombilla = precioBombilla * contadorBombilla;
+    //Cálculo de descuentos
+    function calcularDescuento() {
+        if (cantidadTotalDeProductos >= 3) {
+            mensajeDescuento = "Felicidades, por su compra obtuvo un 10% de descuento";
+            precioConDescuento = sumaFinalPrecios / descuentoDel10;
+        } else if (cantidadTotalDeProductos == 2) {
+            mensajeDescuento = "Cómpre 3 unidades o más y obtendrá un 10% de descuento";
+            precioConDescuento = 0;
+        } else {
+            mensajeDescuento = "No hay descuento";
+            precioConDescuento = 0;
+        }
+        precioFinal = sumaFinalPrecios - precioConDescuento;
+    }
 
-  sumaFinalPrecios = precioFinalTermo + precioFinalMate + precioFinalBombilla;
+    
+    //Mostrar compra final
+    function mostrarProductos(){
+        mensaje = `Usted compró: <br>
+        ${contadorTermo} termo/s, <br>
+        ${contadorMate} mate/s, <br>
+        ${contadorBombilla} bombillas/s, <br>
+        El precio final a pagar es de $${precioFinal}
+        ${mensajeDescuento}`;
 
-  cantidadTotalDeProductos = contadorTermo + contadorMate + contadorBombilla;
+        document.write(mensaje);
+    }
 
-
-  if (cantidadTotalDeProductos >= 3) {
-    mensajeDescuento = 'Felicidades, por su compra obtuvo un 10% de descuento';
-    precioConDescuento = sumaFinalPrecios/descuentoDel10;
-  }
-  else if (cantidadTotalDeProductos == 2) {
-    mensajeDescuento = 'Compre 3 unidades o mas y obtendrá un 10% de descuento';
-    precioConDescuento=0;
-  } else {
-    mensajeDescuento = 'No hay descuento';
-    precioConDescuento=0;
-  }
-
-
-  precioFinal = sumaFinalPrecios - precioConDescuento;
-
-  //MENSAJE FINAL
-  mensaje = `Usted compró ${contadorTermo} unidad/es de termos, <br>
-  ${contadorMate} unidad/es de mates,<br>
-  ${contadorBombilla} unidad/es de bombillas,<br>
-  ${mensajeDescuento} El precio final a pagar es de ${precioFinal}`;
-
-  document.write(mensaje);
-
-}
-
-calcularPrecios();
+//Invocar funciones
+buscarProductos();
+ingresarProductos();
+calcularProductos();
+calcularDescuento();
+mostrarProductos();
