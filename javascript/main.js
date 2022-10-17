b1a70dcfc7efb4afa28f02796bc6f2c971b5e7fa
Gonzalo Reynoso <gonzalo.reynoso9@gmail.com>
2022-10-17 20:49:03 -0300

    inclusion sass

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,5 @@
-//2DA PRE ENTREGA FINAL GONZALO REYNOSO
-//STOCK DE PRODUCTOS
-const stockProductos = [
+//GONZALO REYNOSO
 
-    { id: 1, nombre: "Termo Stanley", marca: "Stanley", presentacion: "940ml", precio: 20, descripcion: "mejor termo", imagen: "https://www.stanleypmi.com.py/wp-content/uploads/2021/09/1-13.jpg" },
-    { id: 2, nombre: "Mate Stanley", marca: "Stanley", presentacion: "240ml", precio: 10, descripcion: "mejor mate", imagen: "https://http2.mlstatic.com/D_NQ_NP_2X_615433-MLA46334848707_062021-F.webp" },
-    { id: 3, nombre: "Bombilla Stanley", marca: "Stanley", presentacion: "18cm", precio: 20, descripcion: "mejor bombilla", imagen: "https://http2.mlstatic.com/D_NQ_NP_2X_874452-MLA49513557450_032022-F.webp" },
-    { id: 4, nombre: "Linterna Indestructible Varta", marca: "VARTA", presentacion: "F20 Pro", precio: 20, descripcion: "linterna", imagen: "https://www.pescatienda.com/large/LINTERNA-INDESTRUCTIBLE-F10-PRO-VARTA-i30739.jpg" },
-    { id: 5, nombre: "", marca: "", presentacion: "LED110 Lumens", precio: 20, descripcion: "linterna", imagen: "https://www.pescatienda.com/large/LINTERNA-FRONTAL-1-LED-110-LUMENS-i28429.jpg" },
-    { id: 5, nombre: "Contador de metros", marca: "Yoshikawa", presentacion: "Z046", precio: 20, descripcion: "contador de metros", imagen: "https://www.pescatienda.com/large/Z046-CONTADOR-DE-METROS-YOSHIKAWA-%28Sin-caja%29-i30626.jpg" }
-];
 
 //CONSTANTES
 const contenedorProductos = document.getElementById("contenedor-productos");
@@ -58,7 +49,7 @@ botonCerrarSesion.addEventListener("click", () => {
 
 //RENDERIZACION DE CARDS POR HTML
 let carrito = [];
-stockProductos.forEach(producto => {
+stockTelevisores.forEach(producto => {
     const renderizado = document.createElement("div");
     renderizado.innerHTML =
         `
@@ -78,14 +69,15 @@ stockProductos.forEach(producto => {
 
 //ACA TRATÉ DE CREAR UN INNERHTML PARA MOSTRAR EL CARRITO SELECCIONADO
 
-// carrito.forEach(producto => {
+// carrito.forEach(carrito => {
 //     const productoComprado = document.createElement("div");
 //     productoComprado.innerHTML = 
 //     `
-//     Usted compró ${carrito.nombre}, ${carrito.marca}, 
-//     ${carrito.presentacion}, y el precio total a pagar es de $${carrito.precio}
+//     <p>Usted compró ${carrito.nombre}, ${carrito.marca}, 
+//     ${carrito.presentacion}, y el precio total a pagar es de $${carrito.precio}<p/>
 //     `
 //     contenedorMensajeCarrito.append(productoComprado);
+//     boton.addEventListener("click", () => mostrarCarrito(carrito));
 // })
 
 //FUNCION PARA AGREGAR AL CARRITO
