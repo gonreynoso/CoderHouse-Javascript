4254e2e09475718058a1234daec9e9cb8d44a04f
Gonzalo Reynoso <gonzalo.reynoso9@gmail.com>
2022-11-01 11:13:28 -0300

    actualizacion json

--- javascript/main.js
+++ javascript/main.js
@@ -9,6 +9,7 @@ const botonBuscar = document.getElementById("botonBuscar");
 const botonIniciarSesion = document.getElementById("botonIniciarSesion");
 const botonCerrarSesion = document.getElementById("botonCerrarSesion");
 const contenedorMensajeCarrito = document.getElementById("contenedor-mensaje-carrito");
+let carrito = [];
 
 
 //SOLICITAR USUARIO POR LOCALSTORAGE
@@ -16,7 +17,7 @@ botonIniciarSesion.addEventListener("click", () => {
     let usuario;
     let usuarioStorage = localStorage.getItem("usuario");
     if (usuarioStorage) {
-                                                //alerta de SWEETALERT
+        //alerta de SWEETALERT
         swal({
             title: `Bienvenido ${usuario}`,
             text: "Haz ingresado con éxito",
@@ -29,7 +30,7 @@ botonIniciarSesion.addEventListener("click", () => {
             text: 'Ingrese su usuario',
             content: "input",
             button: {
-            text: "Aceptar",
+                text: "Aceptar",
             },
         });
         localStorage.setItem("usuario", usuario);
@@ -39,7 +40,7 @@ botonIniciarSesion.addEventListener("click", () => {
 //FUNCION PARA CERRAR SESION POR LOCALSTORAGE
 botonCerrarSesion.addEventListener("click", () => {
     localStorage.clear();
-                                                //alerta de SWEETALERT
+    //alerta de SWEETALERT
     swal({
         title: `Sesión cerrada con éxito`,
         icon: "success",
@@ -53,48 +54,40 @@ const traerProductos = async () => {
     const contenedorProductosPadre = document.getElementById("contenedor-productos-padre");
     try {
         //codigo peligroso en caso de que el servidor esté caído
-        const response = await fetch("https://raw.githubusercontent.com/gonreynoso/CoderHouse-Javascript/main/javascript/televisores.json");
+        const response = await fetch("https://raw.githubusercontent.com/gonreynoso/CoderHouse-Javascript/main/javascript/data.json");
         const data = await response.json();
 
-    data.forEach(producto => {
-    const renderizado = document.createElement("contenedor-productos-padre");
-    renderizado.innerHTML =
-    `
-    <img class= "imagenCards" src = ${producto.imagen} alt "">
-    <h3>${producto.nombre}</h3>
-    <h4>${producto.marca}</h4>
-    <h4>${producto.presentacion}</h4>
-    <p>${producto.descripcion}<p/><br>
-    <span>Precio: $${producto.precio}<span/><br>
-    <button id=${producto.id}>Agregar al carrito</button><br>
-    `
-    contenedorProductosPadre.append(renderizado);
-    const boton = document.getElementById(producto.id);
-    boton.addEventListener("click", () => agregarAlCarrito(producto));
-    });
+        data.forEach(producto => {
+            const renderizado = document.createElement("contenedor-productos-padre");
+            renderizado.innerHTML =
+                `
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <div class="card">
+                        <img src= ${producto.imagen} class="card-img-top" alt="tarjeta-de-productos">
+                        <div class="card-body">
+                            <h5 class="card-title">${producto.nombre}</h5>
+                            <p class="card-text">${producto.presentacion}.</p>
+                            <span>Precio: $${producto.precio}<span/><br>
+                            <p class="card-text">${producto.descripcion}.</p>
+                            <button id="${producto.id}" class="btn btn-primary">Agregar al carrito</button>
+                    </div>
+                </div>
+            </div>
+            `
+            contenedorProductosPadre.append(renderizado);
+            const boton = document.getElementById(producto.id);
+            boton.addEventListener("click", () => console.log(producto)); 
+            // agregarAlCarrito(producto.id));
+        });
     } catch (error) {
-    console.log(error);
+        console.log(error);
     }
 };
 
 traerProductos();
 
 
-
-
-
-//ACA TRATÉ DE CREAR UN INNERHTML PARA MOSTRAR EL CARRITO SELECCIONADO
-
-// carrito.forEach(carrito => {
-//     const productoComprado = document.createElement("div");
-//     productoComprado.innerHTML = 
-//     `
-//     <p>Usted compró ${carrito.nombre}, ${carrito.marca}, 
-//     ${carrito.presentacion}, y el precio total a pagar es de $${carrito.precio}<p/>
-//     `
-//     contenedorMensajeCarrito.append(productoComprado);
-//     boton.addEventListener("click", () => mostrarCarrito(carrito));
-// })
 
 //FUNCION PARA AGREGAR AL CARRITO
 // Abrir consola y ver la compra efectuada
@@ -115,19 +108,20 @@ const agregarAlCarrito = (producto) => {
             cantidad: 1
         })
     }
-    const boton = document.getElementById(producto.id);
-    boton.addEventListener("click", () => console.log(carrito));
+    // boton = document.getElementById(producto.id);
+    // boton.addEventListener("click", () => console.log(carrito));
+
 }
 
 //FUNCION PARA BUSCAR EN EL CARRITO
 //ERROR NO PUDE HACER EL BUSCADOR
-const buscarProducto = (string) => {
-    let productoBuscado = stockProductos.find(producto => producto.nombre.includes(string));
-    console.log(productoBuscado);
-    inputBusqueda.value = "";
-    console.log(productoBuscado);
+// const buscarProducto = (string) => {
+//     let productoBuscado = stockProductos.find(producto => producto.nombre.includes(string));
+//     console.log(productoBuscado);
+//     inputBusqueda.value = "";
+//     console.log(productoBuscado);
 
-    inputBusqueda.addEventListener("click", () => console.log(botonBuscar.value));
+//     inputBusqueda.addEventListener("click", () => console.log(botonBuscar.value));
 
-}
+// }
 
